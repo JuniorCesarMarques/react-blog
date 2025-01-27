c7797235dc40014da9780458e4a35f123de65fd1
JuniorCesarMarques <juniorcms1995@gmail.com>
2025-01-27 16:11:50 -0300

    footer pronto

--- src/components/layout/footer/Footer.jsx
+++ src/components/layout/footer/Footer.jsx
@@ -1,11 +1,28 @@
 import styles from './Footer.module.css';
 
+import { FaInstagram } from "react-icons/fa";
+import { FaYoutube } from "react-icons/fa";
+import { FaTiktok } from "react-icons/fa";
+import { FaLinkedinIn } from "react-icons/fa";
+import { FaWhatsapp } from "react-icons/fa";
+
 const Footer = () => {
   const year = new Date().getFullYear();
 
   return (
     <footer className={styles.container}>
-        <p>&copy; {year} Brothers Loucos por Adrenalina. Todos os direitos reservados.</p>
+        <p className={styles.copyright}>Copyright &copy; {year} Brothers Loucos por Adrenalina. Todos os direitos reservados.</p>
+        <div className={styles.policy_container}>
+          <p>Termos de seviço</p>
+          <p>Políticas de privacidade</p>
+        </div>
+        <div className={styles.social_media}>
+          <FaInstagram />
+          <FaYoutube />
+          <FaTiktok />
+          <FaLinkedinIn />
+          <FaWhatsapp />
+        </div>
     </footer>
   )
 }
